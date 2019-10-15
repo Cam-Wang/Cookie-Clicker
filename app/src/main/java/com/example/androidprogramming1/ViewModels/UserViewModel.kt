dbb6d8c901031547b1e1bcfd745c03c7703c704a
S3TO <ninja00000001@gmail.com>
2019-10-14 18:04:04 -0700

    Create UserViewModel.kt

---
+++ app/src/main/java/com/example/androidprogramming1/ViewModels/UserViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.androidprogramming1.ViewModels
+
+class UserViewModel : ViewModel() {
+    private val users: MutableLiveData<List<User>> by lazy {
+        MutableLiveData().also {
+            loadUsers()
+        }
+    }
+
+    fun getUsers(): LiveData<List<User>> {
+        return users
+    }
+
+    private fun loadUsers() {
+        // Do an asynchronous operation to fetch users.
+    }
+}
