011cc8d81f64324ccd82259b69b5cf2bf5199f00
Cam Wang <wang348@mail.chapman.edu>
2019-11-11 23:25:37 -0800

    api work

--- app/src/main/java/com/example/androidprogramming1/GetData.kt
+++ app/src/main/java/com/example/androidprogramming1/GetData.kt
@@ -1,8 +1,24 @@
 package com.example.androidprogramming1
 
+import retrofit2.Retrofit
 import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.converter.gson.GsonConverterFactory
 
 interface GetData {
 
-    @GET()
+    @GET("v1/gifs/random")
+    fun getWeather(@Query("tag") tag: String,
+                   @Query("api_key") apiKey:String,
+                   @Query("rating") rating: String)
+    companion object {
+        fun create(): gifAPI {
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(
+                    GsonConverterFactory.create())
+                .baseUrl("api.giphy.com")
+                .build()
+            return retrofit.create(gifAPI::class.java)
+        }
+    }
 }
